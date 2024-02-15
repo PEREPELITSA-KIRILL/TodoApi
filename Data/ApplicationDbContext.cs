5b31cf64e1f3057387d452ea2ed75d6688a41518
Kirill Perepelitsa <kiro.040504.oficial@gmail.com>
2024-02-15 17:41:53 +0300

    2 commit

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleTodoApi.Models;
 
+//Контекст  базы данных
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
